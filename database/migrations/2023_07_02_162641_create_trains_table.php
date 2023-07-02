747dfde7571522057196539c1d4a1a9b95b0b0d9
vincenzodivita <divitavincenzo@icloud.com>
2023-07-02 18:36:24 +0200

    Add columns

--- database/migrations/2023_07_02_162641_create_trains_table.php
+++ database/migrations/2023_07_02_162641_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Azienda', 50);
+            $table->string('Stazione_di_partenza', 50);
+            $table->string('Stazione_di_arrivo', 50);
+            $table->string('Orario_di_partenza',50);
+            $table->string('Orario_di_arrivo',50);
+            $table->string('Codice_Treno', 50);
+            $table->tinyInteger('Numero_Carrozze');
+            $table->boolean('In_orario');
+            $table->boolean('Cancellato');
             $table->timestamps();
         });
     }
