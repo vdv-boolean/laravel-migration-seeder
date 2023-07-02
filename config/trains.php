c8c3c4426e61e51e8aa49605a8a4f36dee3ae0d1
vincenzodivita <divitavincenzo@icloud.com>
2023-07-02 20:05:43 +0200

    Fix return

--- config/trains.php
+++ config/trains.php
@@ -1,17 +1,15 @@
 <?php
 
 return [
-    $trains = [
-        [
-            'Azienda'=> 'Trenitalia',
-            'Stazione_di_partenza' => 'Palermo Centrale',
-            'Stazione_di_arrivo' => 'Palermo Notarbartolo',
-            'Orario_di_partenza' => '08:05',
-            'Orario_di_arrivo' => '08:35',
-            'Codice_Treno' => '454647',
-            'Numero_Carrozze' => '1',
-            'In_orario' => true,
-            'Cancellato' => false,
-        ]
+    [
+        'Azienda'=> 'Trenitalia',
+        'Stazione_di_partenza' => 'Palermo Centrale',
+        'Stazione_di_arrivo' => 'Palermo Notarb.',
+        'Orario_di_partenza' => '08:05',
+        'Orario_di_arrivo' => '08:35',
+        'Codice_Treno' => '454647',
+        'Numero_Carrozze' => '20',
+        'In_orario' => true,
+        'Cancellato' => false,
     ]
 ];
