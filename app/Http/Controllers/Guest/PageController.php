86488791d0b242907c3cbca5254f22ec25cf03e1
vincenzodivita <divitavincenzo@icloud.com>
2023-07-03 23:15:48 +0200

    Add fn

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index() {
+        $trains = Train::all();
+        return view('index', [
+            'trains' => $trains,
+        ]);
+    }
 }
